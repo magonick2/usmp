31e2612381c9350985fadb4d27d57377f5249518
gonzalo <gonzalo_cisterna@usmp.pe>
2026-04-25 10:16:49 -0500

    Pregunta 2: Controlador de Cursos con filtros y detalles finalizado

--- Program.cs
+++ Program.cs
@@ -27,6 +27,25 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    // Asegurarse de que la base de datos exista
+    context.Database.EnsureCreated();
+
+    if (!context.Cursos.Any())
+    {
+        context.Cursos.AddRange(
+            new Curso { Codigo = "PROG1", Nombre = "Programación I", Creditos = 4, CupoMaximo = 20, HorarioInicio = new TimeSpan(8, 0, 0), HorarioFin = new TimeSpan(10, 0, 0), Activo = true },
+            new Curso { Codigo = "BD1", Nombre = "Base de Datos I", Creditos = 3, CupoMaximo = 15, HorarioInicio = new TimeSpan(10, 0, 0), HorarioFin = new TimeSpan(12, 0, 0), Activo = true },
+            new Curso { Codigo = "WEB1", Nombre = "Desarrollo Web", Creditos = 5, CupoMaximo = 25, HorarioInicio = new TimeSpan(14, 0, 0), HorarioFin = new TimeSpan(16, 0, 0), Activo = true }
+        );
+        context.SaveChanges();
+    }
+}
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
